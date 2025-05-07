f671a5ab9b4d87d0a06622c56180f4fd12d49c55
Wilbert <wilbert.blogspot@gmail.com>
2025-05-07 09:21:15 +0700

    finish loop string exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (string, number) {
+  let newString = "";
+  if (number < 0) {
+    return "ERROR";
+  }
+  for (let i = 0; i < number; i++) {
+    newString += string;
+  }
+  return newString;
 };
 
+repeatString("hey", 3);
 // Do not edit below this line
 module.exports = repeatString;
