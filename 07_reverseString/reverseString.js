8ae7c9f05b716f6fd63983ec5d36ec81296da1d1
Wilbert <wilbert.blogspot@gmail.com>
2025-05-07 09:45:40 +0700

    finish filter and forEach array to remove from array exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  const arr = string.split("");
+  const arrReverse = arr.reverse();
+  return arrReverse.join("");
 };
 
 // Do not edit below this line
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+  //   let newArr = [];
+  //   array.forEach((item) => {
+  //     if (!args.includes(item)) {
+  //       newArr.push(item);
+  //     }
+  //   });
+  //   return newArr;
+
+  return array.filter((item) => {
+    return !args.includes(item);
+  });
 };
 
 // Do not edit below this line
