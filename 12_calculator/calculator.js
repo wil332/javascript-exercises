edbcfc31f60541c848401531d5ee871a6823326c
Wilbert <wilbert.blogspot@gmail.com>
2025-05-09 17:30:27 +0700

    calculator exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (x) {
+  let sum = 0;
+  for (num of x) {
+    sum += num;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (x) {
+  let multi = 1;
+  for (let i = 0; i < x.length; i++) {
+    multi *= x[i];
+  }
+  return multi;
 };
 
-const power = function() {
-	
+const power = function (x, y) {
+  return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function (x) {
+  let result = 1;
+  for (let i = x; i >= 1; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +41,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
