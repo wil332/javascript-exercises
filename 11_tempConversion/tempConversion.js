5c647c55a383058fd040ff9ce584bce3b78abe68
Wilbert <wilbert.blogspot@gmail.com>
2025-05-09 17:06:21 +0700

    convert celcius to faranheit and the opposite

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (num) {
+  const res = ((num - 32) * 5) / 9;
+  const round = Math.round(res * 10) / 10;
+  return Number.isInteger(round) ? round : round;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (num) {
+  const res = (num * 9) / 5 + 32;
+  const round = Math.round(res * 10) / 10;
+  return Number.isInteger(round) ? round : round;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
