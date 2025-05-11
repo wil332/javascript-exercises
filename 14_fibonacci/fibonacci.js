f2c135d673a8e4ee5877eb319f680cf5e22cfe4d
Wilbert <wilbert.blogspot@gmail.com>
2025-05-11 13:04:36 +0700

    fibonacci exercise finish

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function (countArgs) {
+  let count;
+  if (typeof countArgs !== "number") {
+    count = parseInt(countArgs);
+  } else {
+    count = countArgs;
+  }
 
+  if (count < 0) return "OOPS";
+  if (count == 0) return 0;
+
+  //   let firstPrev = 1;
+  //   let secondPrev = 0;
+
+  //   for (let i = 2; i <= count; i++) {
+  //     let current = firstPrev + secondPrev;
+  //     secondPrev = firstPrev;
+  //     firstPrev = current;
+  //   }
+  //   return firstPrev;
+  const fib = [0, 1];
+  for (let i = 2; i <= count; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib[count];
 };
 
 // Do not edit below this line
--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,18 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (arr) {
+  for (const book of books) {
+    return book.title;
+  }
 };
 
+const books = [
+  {
+    title: "Belajar SQL",
+    author: "Rudi Hartono",
+  },
+  {
+    title: "Rahasia Sukses",
+    author: "Robert Kiyosaki",
+  },
+];
 // Do not edit below this line
 module.exports = getTheTitles;
