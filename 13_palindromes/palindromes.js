dcb3bbc5a874039b39608bf4ac28ebc1dbe0840e
Wilbert <wilbert.blogspot@gmail.com>
2025-05-11 12:12:29 +0700

    palindrome exercise finish

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const chars = str
+    .toLowerCase()
+    .split("")
+    .filter(function (char) {
+      return (char >= "a" && char <= "z") || (char >= "0" && char <= "9");
+    });
 
+  const rev = [...chars].reverse().join("");
+  const strings = chars.join("");
+  if (rev === strings) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
